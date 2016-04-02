b4e1a49ecd4040c9a83dd2ae7feb279b44c522b0
Cédric Biancheri <cdc.biancheri@gmail.com>
2016-04-01 23:52:57 -0400

    fused vm: Add support of containers in the view
    It is possible to highlight specific containers.
    The selection is done with the Select Machine button. The list of machines will
    contain all the detected namespaces. Including the root namespace.
    Currently this version doesn't see the difference between two namespaces with
    the same ID. This can happen with containers on multiple machines.
    
    The algorithm to handle the statedump_process events was rewritten. It creates
    empty attributes in the sht that can be filled later.

--- lttng/org.eclipse.tracecompass.lttng2.kernel.core/src/org/eclipse/tracecompass/internal/lttng2/kernel/core/analysis/vm/module/FusedVMInformationProvider.java
+++ lttng/org.eclipse.tracecompass.lttng2.kernel.core/src/org/eclipse/tracecompass/internal/lttng2/kernel/core/analysis/vm/module/FusedVMInformationProvider.java
@@ -3,8 +3,13 @@ package org.eclipse.tracecompass.internal.lttng2.kernel.core.analysis.vm.module;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.eclipse.jdt.annotation.Nullable;
 import org.eclipse.tracecompass.internal.lttng2.kernel.core.analysis.vm.Attributes;
 import org.eclipse.tracecompass.statesystem.core.ITmfStateSystem;
+import org.eclipse.tracecompass.statesystem.core.ITmfStateSystemBuilder;
+import org.eclipse.tracecompass.statesystem.core.exceptions.AttributeNotFoundException;
+import org.eclipse.tracecompass.statesystem.core.exceptions.StateSystemDisposedException;
+import org.eclipse.tracecompass.statesystem.core.statevalue.ITmfStateValue;
 
 public class FusedVMInformationProvider {
 
@@ -24,6 +29,35 @@ public class FusedVMInformationProvider {
     public static Integer getNbCPUs(ITmfStateSystem ssq, String machineName) {
         List<Integer> vCpuquarks = ssq.getQuarks(Attributes.MACHINES, machineName, "*"); //$NON-NLS-1$
         return vCpuquarks.size();
+    }
+
+    public static int getNodeThreadsAndAdd(ITmfStateSystemBuilder ssq) {
+        return ssq.getQuarkAbsoluteAndAdd(Attributes.THREADS);
+    }
+
+    public static int getNodeThreads(ITmfStateSystem ssq) {
+        try {
+            return ssq.getQuarkAbsolute(Attributes.THREADS);
+        } catch (AttributeNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+    public static @Nullable ITmfStateValue getTypeMachine(ITmfStateSystem ssq, String machineName) {
+        int quark;
+        try {
+            quark = ssq.getQuarkAbsolute(Attributes.MACHINES, machineName);
+            return ssq.querySingleState(ssq.getStartTime(), quark).getStateValue();
+        } catch (AttributeNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (StateSystemDisposedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return null;
     }
 
 }
